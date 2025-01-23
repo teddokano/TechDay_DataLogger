4beaed3ca6edca644754721cadb10b412cea9731
Tedd OKANO <okano.akifumi@gmail.com>
2025-01-23 20:38:18 +0900

    trying to make cookie operational

--- logger.py
+++ logger.py
@@ -6,6 +6,10 @@ import urllib.request
 import json
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+from http.cookies import SimpleCookie
+from http.server import BaseHTTPRequestHandler
+from http.cookies import SimpleCookie
+
 
 html_source = """
 <!DOCTYPE html>
@@ -87,9 +91,20 @@ html_source = """
 			ポップアップメニューを選択し，必要があればテキスト入力欄を埋めて「送信」してください. 
         </section>
         <section id="text-entry">
+
+
+
+
+
             <form id="data-form">
+                <label for="demo">Select a demo:</label>
+                <select id="options" name="demo">
+                    ===DEMO_LIST===
+                </select>
+
                 <label for="tag">TAG:</label>
                 <input id="tag" name="tag" type="number" value="===TAG_ID===" readonly />
+
                 <label for="options">Choose an option:</label>
                 <select id="options" name="options">
                     <option value="option1">Option 1</option>
@@ -115,28 +130,69 @@ html_source = """
 
 PORT = 8000
 
-class ExampleHandler( http.server.SimpleHTTPRequestHandler ):
+class ExampleHandler( BaseHTTPRequestHandler ):
 	def do_GET( self ):
+	
+		cookies = SimpleCookie( self.headers.get( "Cookie" ) )
 		parsed	= urlparse( self.path )
 		query	= parse_qs( parsed.query )
 
-		print( parsed )
-		print( query )
-
 		try:
+			tag_id	= cookies['tag_id'].value
 			tag_id  = query[ "tag" ][0]
 		except KeyError:
 			tag_id  = 9999
 
-		self.send_response(200)
-		self.send_header("Content-Type", "text/html")
-		self.end_headers()
-		self.wfile.write( html_source.replace( '===TAG_ID===', str( tag_id ) ).encode("utf-8") )
+		try:
+			demo_id	= cookies['demo_id'].value
+			demo_id  = query[ "demo" ][0]
+		except KeyError:
+			demo_id  = "demo10"
 
-def serve_rating_page( tag_number ):
-	return "<http><title></title><body>{tag_number}</body></http>"
+		cookies['tag_id' ]	= tag_id
+		cookies['demo_id']	= demo_id
+
+		self.send_response(200)
+		self.send_header( "Set-Cookie", f"tag_id={tag_id};   path=/" )
+		self.send_header( "Set-Cookie", f"demo_id={demo_id}; path=/" )
+		'''
+		C = SimpleCookie()
+		C[ "tag_id"  ] = tag_id
+		C[ "demo_id" ] = demo_id
+		self.send_header( C )
+		'''
+		self.send_header( "Content-Type", "text/html" )
+		self.end_headers()
+		
+		h	= html_source.replace( '===TAG_ID===', str( tag_id ) )
+		h	= h.replace( '===DEMO_LIST===', demo_list( demo_id, 18 ) )
+		self.wfile.write( h.encode("utf-8") )
+
+def demo_list( selected, length ):
+	str_list    = []
+	
+	for i in range( 1, length ):
+		id	= f"demo{i}"
+		if selected == id:
+			sel	= "selected"
+		else:
+			sel	= ""
+    		
+		str_list   += [ f'<option value= "{id}" {sel}>Demo {i}</option>' ]
+
+	return "\n".join( str_list )
+
+
 
 with socketserver.TCPServer(("", PORT), ExampleHandler) as httpd:
+	cookie_expire_seconds	= 3600 * 24 * 3
+	C = SimpleCookie()
+	C[ "tag_id"  ] = 9876
+	C[ "demo_id" ] = "demo99"
+	C[ "tag_id"  ][ "max-age" ] = cookie_expire_seconds
+	C[ "demo_id" ][ "max-age" ] = cookie_expire_seconds
+	print( C )
+		
 	print("serving at port", PORT)
 	httpd.serve_forever()
 
