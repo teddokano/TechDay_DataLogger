bcbf3694ed9e840562f89fc5a62684305b0fc1fe
Tedd OKANO <okano.akifumi@gmail.com>
2025-01-24 05:06:55 +0900

    general file handling added and UI improvements

--- logger.py
+++ logger.py
@@ -10,6 +10,10 @@ from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler
 from http.cookies import SimpleCookie
 
+import	os
+
+
+
 
 html_source = """
 <!DOCTYPE html>
@@ -80,30 +84,33 @@ html_source = """
     </style>
 </head>
 <body>
-    <header>
-        <h4>The TechDay Data Logger</h4>
-    </header>
     <main>
         <section id="selection-menu">
-            <h2>Tag ID: ===TAG_ID===</h2>
+            Tag ID: ===TAG_ID===
+            <img src="img/1234.jpg" style="width: 90%;" alt="Image"><br />
+            職種：製品企画(デモ用固定値)<br />
+            担当製品：犬小屋(デモ用固定値)
         </section>
-        <section id="selection-menu">
-			ポップアップメニューを選択し，必要があればテキスト入力欄を埋めて「送信」してください. 
-        </section>
-        <section id="text-entry">
 
 
-
-
+        <section id="main-input">
 
             <form id="data-form">
-                <label for="demo">Select a demo:</label>
-                <select id="options" name="demo">
-                    ===DEMO_LIST===
+                <label for="chance">採用可能性:</label>
+                <select id="chance" name="chance">
+                    <option value="big">大</option>
+                    <option value="mid">中</option>
+                    <option value="small">小</option>
                 </select>
 
-                <label for="tag">TAG:</label>
-                <input id="tag" name="tag" type="number" value="===TAG_ID===" readonly />
+
+                <label for="interest">技術的関心:</label>
+                <select id="interest" name="interest">
+                    <option value="big">大</option>
+                    <option value="mid">中</option>
+                    <option value="small">小</option>
+                </select>
+
 
                 <label for="options">Choose an option:</label>
                 <select id="options" name="options">
@@ -111,18 +118,38 @@ html_source = """
                     <option value="option2">Option 2</option>
                     <option value="option3">Option 3</option>
                 </select>
-                <label for="data-input">Enter text data:</label>
-                <input type="text" id="data-input" name="data-input" required>
+
+
+                <label for="data-input">メモ:</label>
+                <input type="text" id="data-input" name="data-input">
                 <button type="submit">送信</button>
+
+
+        </section>
+
+        <section id="customize-input">
+
+                <label for="tag">タグ番号手入力:</label>
+                <input id="tag" name="tag" type="number" value="===TAG_ID==="/>
+
+                <label for="demo">担当デモ選択:</label>
+                <select id="options" name="demo">
+                    ===DEMO_LIST===
+                </select>
+
             </form>
         </section>
+    <!--
         <section id="data-display">
 			Something to add
         </section>
+	-->
     </main>
+    <!--
     <footer>
         <p>2025 Tsukimidai Communication Syndicate - Crawl Design</p>
     </footer>
+    -->
 
 </body>
 </html>
@@ -137,36 +164,67 @@ class ExampleHandler( BaseHTTPRequestHandler ):
 		parsed	= urlparse( self.path )
 		query	= parse_qs( parsed.query )
 
-		try:
-			tag_id	= cookies['tag_id'].value
-			tag_id  = query[ "tag" ][0]
-		except KeyError:
-			tag_id  = 9999
-
-		try:
-			demo_id	= cookies['demo_id'].value
-			demo_id  = query[ "demo" ][0]
-		except KeyError:
-			demo_id  = "demo10"
-
-		cookies['tag_id' ]	= tag_id
-		cookies['demo_id']	= demo_id
-
-		self.send_response(200)
-		self.send_header( "Set-Cookie", f"tag_id={tag_id};   path=/" )
-		self.send_header( "Set-Cookie", f"demo_id={demo_id}; path=/" )
-		'''
-		C = SimpleCookie()
-		C[ "tag_id"  ] = tag_id
-		C[ "demo_id" ] = demo_id
-		self.send_header( C )
-		'''
-		self.send_header( "Content-Type", "text/html" )
-		self.end_headers()
+		print( f"parsed = {parsed}" )
+		print( f"parsed.path = {parsed.path}" )
+		print( f"query = {query}" )
 		
-		h	= html_source.replace( '===TAG_ID===', str( tag_id ) )
-		h	= h.replace( '===DEMO_LIST===', demo_list( demo_id, 18 ) )
-		self.wfile.write( h.encode("utf-8") )
+		if parsed.path != "/":
+			print( f"file is there = {os.path.isfile( parsed.path[ 1: ] )}" )
+			print( f"os.path.splitext() = {os.path.splitext( parsed.path )}" )
+			
+			file_path	= parsed.path[ 1: ]
+			
+			if not os.path.isfile( file_path ):
+				self.send_response( 404 )
+				self.wfile.write( f"file not found: \"{file_path}\"" )
+			else:
+				try:
+					content_type = {ext_content[ os.path.splitext( parsed.path )[ 1 ][ 1: ] ]}
+				except KeyError:
+					content_type  = "text/html"
+
+				with open( file_path, "rb" ) as f:
+					data 	= f.read()					
+
+				print( f"ext_content = {ext_content[ os.path.splitext( parsed.path )[ 1 ][ 1: ] ]}" )
+		
+				self.send_response(200)
+				self.send_header( "Content-Type", content_type )
+				self.end_headers()
+			
+				self.wfile.write( data )
+
+		else:
+
+			try:
+				tag_id	= cookies['tag_id'].value
+				tag_id  = query[ "tag" ][0]
+			except KeyError:
+				tag_id  = 9999
+
+			try:
+				demo_id	= cookies['demo_id'].value
+				demo_id  = query[ "demo" ][0]
+			except KeyError:
+				demo_id  = "demo10"
+
+			cookies['tag_id' ]	= tag_id
+			cookies['demo_id']	= demo_id
+
+			cookie_expire_seconds	= 3600 * 24 * 3
+			cookies[ "tag_id"  ][ "max-age" ] = cookie_expire_seconds
+			cookies[ "demo_id" ][ "max-age" ] = cookie_expire_seconds
+
+			self.send_response(200)
+			self.send_header( "Set-Cookie", f"tag_id={tag_id};   path=/" )
+			self.send_header( "Set-Cookie", f"demo_id={demo_id}; path=/" )
+
+			self.send_header( "Content-Type", "text/html" )
+			self.end_headers()
+			
+			h	= html_source.replace( '===TAG_ID===', str( tag_id ) )
+			h	= h.replace( '===DEMO_LIST===', demo_list( demo_id, 18 ) )
+			self.wfile.write( h.encode("utf-8") )
 
 def demo_list( selected, length ):
 	str_list    = []
@@ -183,18 +241,22 @@ def demo_list( selected, length ):
 	return "\n".join( str_list )
 
 
-
-with socketserver.TCPServer(("", PORT), ExampleHandler) as httpd:
-	cookie_expire_seconds	= 3600 * 24 * 3
-	C = SimpleCookie()
-	C[ "tag_id"  ] = 9876
-	C[ "demo_id" ] = "demo99"
-	C[ "tag_id"  ][ "max-age" ] = cookie_expire_seconds
-	C[ "demo_id" ][ "max-age" ] = cookie_expire_seconds
-	print( C )
-		
-	print("serving at port", PORT)
-	httpd.serve_forever()
+def main():
+	with socketserver.TCPServer(("", PORT), ExampleHandler) as httpd:		
+		print("serving at port", PORT)
+		httpd.serve_forever()
 
 
-	
+ext_content	= {	"css" : "text/css",
+				"html": "text/html",
+				"js"  : "text/javascript",
+				"png" : "image/png",
+				"jpg" : "image/jpg",
+				"ico" : "image/ico",
+				}	
+
+
+
+
+if __name__ == "__main__":
+	main()
